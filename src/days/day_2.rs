e16b4947c482bca6aa64533e161ffa9185fc3e68
QuaternionWake <simep@proton.me>
2024-12-03 02:47:10 +0100

    Solve day 2 part 2

--- src/days/day_2.rs
+++ src/days/day_2.rs
@@ -15,7 +15,7 @@ pub fn run() {
     }
 
     let mut safe_count = 0;
-    for report in reports {
+    for report in &reports {
         if report.len() < 2 {
             safe_count += 1;
             continue;
@@ -37,4 +37,134 @@ pub fn run() {
     }
 
     println!("Safe report count: {safe_count}");
+
+    // Simpler O(n*m^2) solution
+
+    // let mut safe_count = 0;
+    // for report in &reports {
+    //     for i in 0..report.len() {
+    //         let mut report = report.clone();
+    //         report.remove(i);
+
+    //         if report.len() < 2 {
+    //             safe_count += 1;
+    //             break;
+    //         }
+
+    //         match report[0].cmp(&report[1]) {
+    //             std::cmp::Ordering::Equal => continue,
+    //             std::cmp::Ordering::Less => {
+    //                 if report.windows(2).all(|x| (1..=3).contains(&(x[1] - x[0]))) {
+    //                     safe_count += 1;
+    //                     break;
+    //                 }
+    //             }
+    //             std::cmp::Ordering::Greater => {
+    //                 if report.windows(2).all(|x| (1..=3).contains(&(x[0] - x[1]))) {
+    //                     safe_count += 1;
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    // println!("Safe report with dampener count: {safe_count}");
+
+    let mut safe_count = 0;
+    for report in &reports {
+        if report.len() < 3 {
+            safe_count += 1;
+            continue;
+        }
+
+        let mut is_safe = true;
+        let mut dampener_available = true;
+        let mut skip_next = false;
+        if !(1..=3).contains(&(report[0] - report[1])) {
+            if (1..=3).contains(&(report[0] - report[2])) {
+                dampener_available = false;
+                skip_next = true;
+            } else {
+                dampener_available = false;
+            }
+        }
+        for chunk in report.windows(4) {
+            if skip_next {
+                skip_next = false;
+                continue;
+            }
+            if (1..=3).contains(&(chunk[1] - chunk[2])) {
+                continue;
+            }
+            if !dampener_available {
+                is_safe = false;
+                break;
+            }
+            if (1..=3).contains(&(chunk[1] - chunk[3])) {
+                dampener_available = false;
+                skip_next = true;
+            } else if (1..=3).contains(&(chunk[0] - chunk[2])) {
+                dampener_available = false;
+            } else {
+                is_safe = false;
+                break;
+            }
+        }
+        if !(1..=3).contains(&(report[report.len() - 2] - report[report.len() - 1]))
+            && !skip_next
+            && !dampener_available
+        {
+            is_safe = false;
+        }
+        if is_safe {
+            safe_count += 1;
+            continue;
+        }
+
+        let mut is_safe = true;
+        let mut dampener_available = true;
+        let mut skip_next = false;
+        if !(1..=3).contains(&(report[1] - report[0])) {
+            if (1..=3).contains(&(report[2] - report[0])) {
+                dampener_available = false;
+                skip_next = true;
+            } else {
+                dampener_available = false;
+            }
+        }
+        for chunk in report.windows(4) {
+            if skip_next {
+                skip_next = false;
+                continue;
+            }
+            if (1..=3).contains(&(chunk[2] - chunk[1])) {
+                continue;
+            }
+            if !dampener_available {
+                is_safe = false;
+                break;
+            }
+            if (1..=3).contains(&(chunk[3] - chunk[1])) {
+                dampener_available = false;
+                skip_next = true;
+            } else if (1..=3).contains(&(chunk[2] - chunk[0])) {
+                dampener_available = false;
+            } else {
+                is_safe = false;
+                break;
+            }
+        }
+        if !(1..=3).contains(&(report[report.len() - 1] - report[report.len() - 2]))
+            && !skip_next
+            && !dampener_available
+        {
+            is_safe = false;
+        }
+        if is_safe {
+            safe_count += 1;
+        }
+    }
+
+    println!("Safe report with dampener count: {safe_count}");
 }
